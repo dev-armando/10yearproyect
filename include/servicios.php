de1812ea784ff10a51f634431d3a2710aa0f8312
Armando Perdomo <armandoperdomoing@gmail.com>
2018-05-04 21:34:41 -0430

    servicios listo

--- include/servicios.php
+++ include/servicios.php
@@ -4,78 +4,56 @@
             <h2 class="infotxt">Aprende y crece de la mano de un reconocido equipo de asistencia técnica y con el apoyo de una creciente comunidad</h2>
             <p class="infotxm">No estarás solo. Domina la metodología inbound y saca el máximo partido a tus herramientas con el valioso apoyo del equipo de asistencia técnica de HubSpot y una comunidad de miles de profesionales del marketing y las ventas.</p>
         </div>
-        <div class="row light-shadow">
-            <div class="central">
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="community-box">
-                        <h1>
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                            <div class="separator"></div>
-                            Más de 150
-                        </h1>
-                        <p>Grupos de Usuarios de NerdCom</p>
-                        </div>
-                </div>
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="community-box">
-                        <i class="fa fa-bug" aria-hidden="true"></i>
-                        <div class="separator"></div>
-                        <h2>Más de 4.5 mill.</h2>
-                        <p>de visitas mensuales</p>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="community-box">
-                        <i class="fa fa-eye" aria-hidden="true"></i>
-                        <div class="separator"></div>
-                        <h2>Más de 112.000</h2>
-                        <p>profesionales certificados</p>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="community-box">
-                        <i class="fa fa-street-view" aria-hidden="true"></i>
-                        <div class="separator"></div>
-                        <h2>Más de 19.000</h2>
-                        <p>asistentes registrados</p>
-                    </div>
-                </div>
+        <div class="row light-shadow community-block">
+            <div class="col-md-3 col-sm-6 col-xs-12 community-box">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <div class="separator"></div>
+                <h2>Más de 150</h2>
+                <p>Grupos de Usuarios de NerdCom NerdCom NerdCom NerdCom</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 community-box">
+                <i class="fa fa-bug" aria-hidden="true"></i>
+                <div class="separator"></div>
+                <h2>Más de 4.5 mill.</h2>
+                <p>de visitas mensuales</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 community-box">
+                <i class="fa fa-eye" aria-hidden="true"></i>
+                <div class="separator"></div>
+                <h2>Más de 112.000</h2>
+                <p>profesionales certificados</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 community-box last-box">
+                <i class="fa fa-street-view" aria-hidden="true"></i>
+                <div class="separator"></div>
+                <h2>Más de 19.000</h2>
+                <p>asistentes registrados</p>
             </div>
         </div>
-        <div class="row">
-            <div class="central">
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="community-box">
-                        <i class="fa fa-users" aria-hidden="true"></i>
-                        <div class="separator"></div>
-                        <h2>Más de 150</h2>
-                        <p>Grupos de Usuarios de NerdCom</p>
-                    </div>
-                </div>                       
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="community-box">
-                        <i class="fa fa-commenting" aria-hidden="true"></i>
-                        <div class="separator"></div>
-                        <h2 >Más de 4.5 mill.</h2>
-                        <p>de visitas mensuales</p>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="community-box">
-                        <i class="fa fa-connectdevelop" aria-hidden="true"></i>
-                        <div class="separator"></div>
-                        <h2>Más de 112.000</h2>
-                        <p>profesionales certificados</p>
-                    </div>
-                </div>      
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="community-box">
-                        <i class="fa fa-street-view" aria-hidden="true"></i>
-                        <div class="separator"></div>
-                        <h2>Más de 19.000</h2>
-                        <p>asistentes registrados</p>
-                    </div>
-                </div>
+        <div class="row community-block">
+            <div class="col-md-3 col-sm-6 col-xs-12 community-box">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <div class="separator"></div>
+                <h2>Más de 150</h2>
+                <p>Grupos de Usuarios de NerdCom NerdCom NerdCom NerdCom</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 community-box">
+                <i class="fa fa-bug" aria-hidden="true"></i>
+                <div class="separator"></div>
+                <h2>Más de 4.5 mill.</h2>
+                <p>de visitas mensuales</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 community-box">
+                <i class="fa fa-eye" aria-hidden="true"></i>
+                <div class="separator"></div>
+                <h2>Más de 112.000</h2>
+                <p>profesionales certificados</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 community-box last-box">
+                <i class="fa fa-street-view" aria-hidden="true"></i>
+                <div class="separator"></div>
+                <h2>Más de 19.000</h2>
+                <p>asistentes registrados</p>
             </div>
         </div>
     </div>
--- include/tours.php
+++ include/tours.php
@@ -1,28 +1,35 @@
-<section class="section" style="padding-top: 6%;">
-
-        	<div class="contenedor">
-	            
-	            <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 row animate-box rfresponsive tblar" style="width: 100%;  margin-top: 5%;padding-bottom: 30px;">
-
-	                <div class="cimgresponsive2" style="float: right;width: 50%;position: relative;">
-	                    
-	                    <div class="cimgresponsive">
-	                        <img class="img-responsive" src="images/women.png" alt="NerdCom Tours" style=" background-color: #000;">
-	                    </div>
-
-
-	                    <img class="img-responsive imagenburbuja" src="images/burbijas.png" alt="NerdCom aros" style=" "> 
-	                </div>
-	                
-	                <div class="historiapaddingleft tblab" style="width: 50%;float: left;">
-	                    <h2 class="historiatittle"><span>Empieza a crecer </span><span style="color: #44B4C4; font-weight: bold;">¡HOY!</span></h2>
-	                    <p class="historiapletras">Gracias a las múltiples herramientas de Nerdcom pro que te ayudan a centralizar cada aspecto de tu empresa y al incondicional apoyo de un equipo comprometido con tu crecimiento, Administrar tu negocio nunca había sido tan fácil.</p> 
-	                    <p class="historiapletras">Da el primer paso invierte el éxito de tu empresa</p>
-	                    <div class="md-form form-group">
-				        	<a href="/showcase" target="_blank" class="btn btn-lg btncom" role="button">Leer Historia</a>
-				    	</div>
-	                    <small style="font-size: .75em;"> Versiones gratuitas de todos los productos de NerdcomPRO. Comienza ahora y actualiza cada versión en función de tu crecimiento.</small>
-	                </div>
+<section class="section">
+	<div class="contenedor">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 tour-info">
+				<h1><span>Empieza a crecer <strong>¡HOY!</strong></span></h1>
+	            <p class="hidden-sm hidden-xs">Gracias a las múltiples herramientas de Nerdcom pro que te ayudan a centralizar cada aspecto de tu empresa y al incondicional apoyo de un equipo comprometido con tu crecimiento, Administrar tu negocio nunca había sido tan fácil. </p> 
+	            <p>Da el primer paso invierte el éxito de tu empresa</p>
+	            <a href="/showcase" target="_blank" class="btn btn-lg btn-jumbo" role="button">Leer Historia</a>
+	            <small> Versiones <strong>GRATUITAS</strong> de todos los productos de NerdcomPRO. Comienza ahora y actualiza cada versión en función de tu crecimiento.</small>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				<div>
+	                <img src="images/women.png" alt="NerdCom Tours">
 	            </div>
-		    </div>
-        </section>
+	            <img src="images/burbijas.png" alt="NerdCom aros"> 
+			</div>
+		</div>
+	    <!--<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 animate-box rfresponsive tblar">
+			<div class="cimgresponsive2">                    
+	            <div class="cimgresponsive">
+	                <img class="img-responsive" src="images/women.png" alt="NerdCom Tours">
+	            </div>
+	            <img class="img-responsive imagenburbuja" src="images/burbijas.png" alt="NerdCom aros"> 
+	        </div>
+	        <div>
+	            <h1><span>Empieza a crecer <strong>¡HOY!</strong></span></h1>
+	            <p>Gracias a las múltiples herramientas de Nerdcom pro que te ayudan a centralizar cada aspecto de tu empresa y al incondicional apoyo de un equipo comprometido con tu crecimiento, Administrar tu negocio nunca había sido tan fácil. </br> Da el primer paso invierte el éxito de tu empresa</p>
+	            <div class="md-form form-group">
+				    <a href="/showcase" target="_blank" class="btn btn-lg btn-jumbo" role="button">Leer Historia</a>
+				</div>
+	            <small> Versiones gratuitas de todos los productos de NerdcomPRO. Comienza ahora y actualiza cada versión en función de tu crecimiento.</small>
+	        </div>
+	    </div>-->
+	</div>
+</section>
