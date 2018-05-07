d49fc71673d3518cb986afaea3383c3f0b5a49d2
Armando Perdomo <armandoperdomoing@gmail.com>
2018-05-06 21:11:16 -0430

    tour & community
    Responsive listos

--- include/tours.php
+++ include/tours.php
@@ -1,17 +1,21 @@
 <section class="section-tour">
 	<div class="container-responsive">
-		<div class="row">
-			<div class="tour-info">
-				<div class="col-xs-8">
-					<h1>Empieza a crecer <strong>¡HOY!</strong></h1>
-		            <p class="hidden-sm hidden-xs">Gracias a las múltiples herramientas de Nerdcom pro que te ayudan a centralizar cada aspecto de tu empresa y al incondicional apoyo de un equipo comprometido con tu crecimiento, Administrar tu negocio nunca había sido tan fácil. </p> 
-		            <p class="hidden-sm hidden-xs">Da el primer paso invierte el éxito de tu empresa</p>
-		     		<a href="/showcase" target="_blank" class="btn btn-lg btn-jumbo" role="button">Leer Historia</a>
-		            <small> Versiones <strong>GRATUITAS</strong> de todos los productos de NerdcomPRO. Comienza ahora y actualiza cada versión en función de tu crecimiento.</small>
+		<div class="row tour-info">
+			<div class="col-md-4 col-md-push-8 col-sm-12 image-wrapper">
+				<div class="women-wrap">
+					<img src="images/women.png" class="women-img" alt="NerdCom Tours">
+				</div>
+				<div class="bubbles-wrap">
+					<img src="images/burbijas.png" class="bubble-img" alt="NerdCom aros"> 
 				</div>
 			</div>
-			<img src="images/women.png" class="women-img" alt="NerdCom Tours">
-			<img src="images/burbijas.png" class="bubbles-img" alt="NerdCom aros"> 
+			<div class="col-md-8 col-md-pull-4 col-sm-12">
+				<h1>Empieza a crecer <strong>¡HOY!</strong></h1>
+		        <p>Gracias a las múltiples herramientas de Nerdcom pro que te ayudan a centralizar cada aspecto de tu empresa y al incondicional apoyo de un equipo comprometido con tu crecimiento, Administrar tu negocio nunca había sido tan fácil. </p> 
+		        <p>Da el primer paso invierte el éxito de tu empresa</p>
+		     	<a href="/showcase" target="_blank" class="btn btn-lg btn-jumbo" role="button">Leer Historia</a>
+		        <small> Versiones <strong>GRATUITAS</strong> de todos los productos de NerdcomPRO. Comienza ahora y actualiza cada versión en función de tu crecimiento.</small>
+			</div>
 		</div>
 	</div>
 </section>
